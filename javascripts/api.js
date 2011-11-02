79d956b3336c2a855488fbcacb6fc6f13613e6cb
Warren Konkel <wkonkel@gmail.com>
2011-11-02 12:27:20 -0700

    move api into frontend

---
+++ javascripts/api.js
@@ -0,0 +1,331 @@
+var Badger = {
+  api: function() {
+    // parse arguments: url, [http_method], [params], [callback]
+    var args = Array.prototype.slice.call(arguments, 0);
+
+    // TODO: hard-code this and braintreeEncrypt() and make dev/qa work differently
+    var host = document.location.protocol + '//' + document.location.host.replace('www', 'api');
+    var url = host + args.shift();
+    var method = typeof(args[0]) == 'string' ? args.shift() : 'GET';
+    var params = typeof(args[0]) == 'object' ? args.shift() : {};
+    var callback = typeof(args[0]) == 'function' ? args.shift() : function(){};
+    
+    // callback sequence number
+    Badger.callback_sequence = (Badger.callback_sequence || 0) + 1;
+    var callback_name = 'callback_' + Badger.callback_sequence;
+
+    // default params
+    params.cache = (new Date().getTime());
+    params.callback = 'Badger.jsonp_callbacks.' + callback_name;
+    if (method != 'GET') params._method = method;
+    params.access_token = Badger.getAccessToken();
+
+    // params to url string
+    for (var key in params) {
+      url += (url.indexOf('?') == -1 ? '?' : '&');
+      url += key + '=' + escape(params[key]||'');
+    }
+    
+    // TODO: alert if url is too long
+
+    var script = document.createElement("script");        
+    script.async = true;
+    script.type = 'text/javascript';
+    script.src = url;
+
+    Badger.jsonp_callbacks[callback_name] = function(response) {
+      // var error = JSON.parse(data.responseText);
+      // if (response.data && response.data.errors) {
+      //   response.data.errors.forEach(function(err) {
+      //    console.log("Error with " + err.resource + ". Field: '" + err.field + "' Code: '" + err.code + "'");
+      //   });
+      // }
+
+      delete Badger.jsonp_callbacks[callback_name];
+      document.head.removeChild(script);
+
+      if (response && response.meta && response.meta.status == 'unauthorized') {
+        Badger.logout();
+      } else {
+        callback.call(null,response);
+      }
+    };
+
+    document.head.appendChild(script);
+  },
+  jsonp_callbacks: {},
+  
+  // reads from "badger_access_token" cookie
+  getAccessToken: function() {
+    var ca = document.cookie.split(';');
+    for (var i=0; i < ca.length; i++) {
+      while (ca[i].charAt(0)==' ') ca[i] = ca[i].substring(1,ca[i].length);
+      if (ca[i].length > 0) {
+        var kv = ca[i].split('=');
+        if (kv[0] == 'badger_access_token') return kv[1];
+      }
+    }
+  },
+  
+  // writes to "badger_access_token" cookie
+  setAccessToken: function(token) {
+    if (!token) {
+      document.cookie = "badger_access_token=; path=/; expires=Thu, 01-Jan-1970 00:00:01 GMT";
+    } else {
+      document.cookie = "badger_access_token="+token+"; path=/";
+    }
+  },
+
+  onLogin: function(callback) {
+    Badger.login_callbacks.push(callback);
+  },
+  login_callbacks: [],
+  
+  onLogout: function(callback) {
+    Badger.logout_callbacks.push(callback);
+  },
+  logout_callbacks: [],
+  
+  // given email+password, returns an access token
+  login: function(email, password, callback) {
+    Badger.api("/account/get_access_token", 'POST', { email: email, password: password }, function(response) {
+      if (response.meta.status == 'ok') Badger.setAccessToken(response.data.access_token);
+      if (callback) callback(response);
+      for (var i=0; i < Badger.login_callbacks.length; i++) Badger.login_callbacks[i].call(null);
+    });
+  },
+  
+  // erases local cookie
+  logout: function(callback) {
+    Badger.setAccessToken(null);
+    if (callback) callback();
+    for (var i=0; i < Badger.logout_callbacks.length; i++) Badger.logout_callbacks[i].call(null);
+  },
+  
+  accountInfo: function(callback) {
+    Badger.api("/account/info", callback);
+  },
+  
+  requestInvite: function(email, callback) {
+    Badger.api("/account/request_invite", 'POST', { email: email }, callback);
+  },
+
+  createAccount: function(data, callback) {
+    Badger.api("/account", 'POST', data, function(response) {
+      if (response.meta.status == 'ok') Badger.setAccessToken(response.data.access_token);
+      if (callback) callback(response);
+    });
+  },
+  
+  domainSearch: function(query, callback) {
+    Badger.__search_serial_next = (Badger.__search_serial_next || 1) + 1;
+    Badger.__search_serial_last = (Badger.__search_serial_last || 0);
+    
+    Badger.api("/domains/search", { query: query, serial: Badger.__search_serial_next }, function(results) {
+      if (parseInt(results.data.serial) > Badger.__search_serial_last) {
+        Badger.__search_serial_last = parseInt(results.data.serial);
+        callback(results);
+      }
+    });
+  },
+
+  getDomains: function(callback) {
+    Badger.api("/domains", function(response) { callback(response.data); });
+  },
+
+  getDomain: function(name, callback) {
+    Badger.api("/domains/" + name, callback);
+  },
+  
+  
+
+  
+  
+  getRecords: function(name, callback) {
+    Badger.api("/domains/" + name + "/records", function(response) { callback(response.data); });   
+  },
+
+  addRecord: function(name, data, callback) {
+    Badger.api("/domains/" + name + "/records", 'POST', data, function(response) {
+      callback(response);
+    });   
+  },
+
+  deleteRecord: function(name, id, callback) {
+    Badger.api("/domains/" + name + "/records/" + id, "DELETE", function(resp) {
+      callback(resp);
+    });
+  },
+  
+
+  registerDomain: function(data, callback) {
+    Badger.api("/domains", 'POST', data, callback);
+  },
+
+  updateDomain: function(domain, options, callback) {
+    Badger.api("/domains/" + domain, 'POST', options, function(resp) {
+      callback(resp);
+    }); 
+  },
+
+  getContacts: function(callback) {
+    Badger.api("/account/contacts", callback);
+  },
+
+  createContact: function(data, callback) {
+    Badger.api("/account/contacts", 'POST', data, callback);
+  },
+
+	updateContact: function(contact_id, data, callback) {
+		Badger.api("/account/contacts/" + contact_id, 'POST', data, callback);
+	},
+
+
+  braintreeEncrypt: function(data) {
+    if (!Badger.braintree) {
+      Badger.braintree = Braintree.create((document.location.host=='www.badger.com')?"MIIBCgKCAQEA7q46U13zKcfozxhNgL8RxF1LEXm2M1NMyromzybYIuwMnXRKg9/DsbQDls2V1uGv8zhqrS3lMmBIvNR6gv2vAMTSwYDKE2VJcUlJveNAP1q5EMMqwTFTlPoBr8uwhT6Q7919O2UDTVi6t/8dm8k6RZsdL+TDcjc7gpNNK4JHOJbqnKpAKPc9uTpo6IlUan2FJVBRk0GVhrRwR+S1YU9aJW8rHPrMovkUpOUUFhzfgGcrFdbbLUTNkxB4OwKxkwYME82i6UlHNxUoRPYama/Tpn+68gu+oV27nJmAF/iuMsV5iLrCyXzbSvt3YBwOctb+WFIqsc/kSd1SsYKZgh9RWQIDAQAB":"MIIBCgKCAQEAvypnA1PqZrAN2nPiWEcPitpEqF/4spcVFvZjCp9T/34nM8sakWQMm9RfKLj24wLYJlgpTdSfQCsU7jms7XOwFyLAajEWk+bRy5E4GheaLOsQsUvZuzgMr4BncDlL4jwxpgiQ67ngSxp2mOB6qySWHlD7E5kNtkl97Q1WyH3IJlSi3Crd/QCsnafMeJhvziuRGIKIX/QBmblcqRXqfjbkmG9VcLUiwsMYuIwuksm3fWHJBYncHoPeZ29lu4eUXuAwLL78VZqtEbxoP9KqW+8yUfX7JBRUq/e6beTtEYc6bhQ9UWxAXANH8jmFOhTu9cPTyGOmI6BgJdfBEPjWncM7GQIDAQAB");
+    }
+    if( data.cc_number && data.cc_number.length > 0) data.cc_number = Badger.braintree.encrypt(data.cc_number);
+    if( data.cc_cvv && data.cc_cvv.length > 0 ) data.cc_cvv = Badger.braintree.encrypt(data.cc_cvv);
+    return data;
+  },
+
+  getPaymentMethods: function(callback) {
+    Badger.api("/account/payment_methods", callback);
+  },
+
+  purchaseCredits: function(data, callback) {
+		if(!data.payment_method_id) data.payment_method_id = "0"
+		if(!data.cc_expiration_date) data.cc_expiration_date = data.cc_expiration_date_month + "/" + data.cc_expiration_date_year
+		
+    Badger.api("/account/purchase_credits", 'POST', Badger.braintreeEncrypt(data), callback);
+  },
+
+  getPendingTransfers: function(callback) {
+    Badger.api("/domains/pending_transfers", callback);
+  },
+
+  getCreditHistory: function(callback) {
+    Badger.api("/account/credit_history", callback);
+  },
+
+  //ADMIN COMMANDS, requires person to have admin flag set to true
+	
+	isAdmin: function(callback) {
+		Badger.api("/account/is_admin", callback);
+	},
+	
+	getPeopleAdmin: function(callback) {
+		Badger.api("/admin/people", callback);
+	},
+	
+	getPersonAdmin: function(id, callback) {
+		Badger.api("/admin/people/" + id, callback);
+	},
+	
+	getDomainsAdmin: function(callback) {
+		Badger.api("/admin/domains", callback);
+	},
+	
+	getDomainAdmin: function(id, callback) {
+		Badger.api("/admin/domains/" + id, callback);
+	},
+  
+	getTransactionsAdmin: function(callback) {
+		Badger.api("/admin/transactions", callback);
+	},
+	
+	getTransactionAdmin: function(id, callback) {
+		Badger.api("/admin/transactions/" + id, callback);
+	},
+	
+	getPurchaseRecordsAdmin: function(callback) {
+		Badger.api("/admin/purchase_records", callback);
+	},
+	
+	getPurchaseRecordAdmin: function(id, callback) {
+		Badger.api("/admin/purchase_records/" + id, callback);
+	},
+  
+  
+  
+  
+  
+  // getRecordsByType: function(accessToken, name, type, callback) {
+  //   Badger.api("/domains/" + name + "/records/" + type, { headers: { access_token: accessToken } }, function(records) {
+  //     callback(records);
+  //   });
+  // },
+  // 
+  // getRecord: function(accessToken, name, id, callback) {
+  //   Badger.api("/domains/" + name + "/records/" + id, { headers: { access_token: accessToken } }, function(record) {
+  //     callback(record);
+  //   });   
+  // },
+  // 
+  // 
+  // deleteRecordsByType: function(accessToken, name, type, callback) {
+  //   Badger.api("/domains/" + name + "/records/" + type, { type: "DELETE", headers: { access_token: accessToken } }, function(resp) {
+  //     callback(resp);
+  //   });   
+  // },
+  // 
+  // updateRecord: function(accessToken, name, id, data, callback) {
+  //   Badger.api("/domains/" + name + "/records/" + id, { type: "PUT", headers: { access_token: accessToken }, data: data }, function(resp) {
+  //     callback(resp);
+  //   });
+  // },  
+  // 
+  // showAccount: function(accessToken, callback) {
+  //   Badger.api("/account", function(account) {
+  //     callback(account);
+  //   });
+  // },
+  // 
+  // checkEmail: function(email, callback) {
+  //   Badger.api("/account/check_email", 'POST', { email: email }, callback);
+  // },
+  // 
+  // 
+  // updateAccount: function(accessToken, data, callback) {
+  //   Badger.api("/account", "POST", data, callback);
+  // },
+  // 
+  // forgotPassword: function(email, callback) {
+  //   Badger.api("/account/forgot_password", { type: "POST", data: { email: email } }, function(resp) {
+  //     callback(resp);
+  //   });
+  // },
+  // 
+  // resetPassword: function(email, newPassword, code, callback) {
+  //   Badger.api("/account/reset_password", { type: "POST", data: { email: email, new_password: newPassword, code: code} }, function(resp) {
+  //     callback(resp);
+  //   });
+  // },
+  // 
+  // changePassword: function(accessToken, newPassword, callback) {
+  //   Badger.api("/account/change_password", { type: "POST", headers: { access_token: accessToken }, data: { new_password: newPassword } }, function(resp) {
+  //     callback(resp);
+  //   });
+  // },
+  // 
+  // unregisterDomain: function(accessToken, name, callback) {
+  //   Badger.api("/domains/" + name, { type: "DELETE", headers: { access_token: accessToken } }, function(resp) {
+  //     callback(resp);
+  //   });
+  // },
+  // 
+  // renewDomain: function(accessToken, data, callback) {
+  //   Badger.api("/domains/" + data.name + "/renew", { type: "PUT", headers: { access_token: accessToken }, data: data }, function(resp) {
+  //     callback(resp);
+  //   });
+  // },
+  // 
+  // getWhois: function(accessToken, name, callback) {
+  //   Badger.api("/domains/" + name + "/whois", { headers: { access_token: accessToken } }, function(whois) {
+  //     callback(whois);
+  //   });
+  // },
+
+  
+};
---
+++ javascripts/api_cache.js
@@ -0,0 +1,73 @@
+var BadgerCache = {
+  flush: function(key) {
+    if (!key || (key == 'domains')) BadgerCache.cached_domains = null;
+    if (!key || (key == 'payment_methods')) BadgerCache.cached_payment_methods = null;
+    if (!key || (key == 'contacts')) BadgerCache.cached_contacts = null;
+    if (!key || (key == 'account_info')) BadgerCache.cached_account_info = null;
+  },
+
+  reload: function(key) {
+    BadgerCache.flush(key);
+    BadgerCache.load();
+  },
+  
+  load: function() {
+    if (Badger.getAccessToken()) {
+      BadgerCache.getDomains();
+      BadgerCache.getPaymentMethods();
+      BadgerCache.getContacts();
+      BadgerCache.getAccountInfo();
+    }
+  },
+  
+  getAccountInfo: function(callback) {
+    callback = callback || function(){};
+    if (BadgerCache.cached_account_info) {
+      callback(BadgerCache.cached_account_info);
+    } else {
+      Badger.accountInfo(function(results) { 
+        BadgerCache.cached_account_info = results;
+        callback(BadgerCache.cached_account_info);
+      });
+    }
+  },
+
+  getDomains: function(callback) {
+    callback = callback || function(){};
+    if (BadgerCache.cached_domains) {
+      callback(BadgerCache.cached_domains);
+    } else {
+      Badger.getDomains(function(results) { 
+        BadgerCache.cached_domains = results;
+        callback(BadgerCache.cached_domains);
+      });
+    }
+  },
+
+  getPaymentMethods: function(callback) {
+    callback = callback || function(){};
+    if (BadgerCache.cached_payment_methods) {
+      callback(BadgerCache.cached_payment_methods);
+    } else {
+      Badger.getPaymentMethods(function(results) { 
+        BadgerCache.cached_payment_methods = results;
+        console.log("SETTING PAYMENT METHOD");
+        console.log(results);
+        callback(BadgerCache.cached_payment_methods);
+      });
+    }
+  },
+  
+  getContacts: function(callback) {
+    callback = callback || function(){};
+    if (BadgerCache.cached_contacts) {
+      callback.call(null,BadgerCache.cached_contacts);
+    } else {
+      Badger.getContacts(function(results) { 
+        BadgerCache.cached_contacts = results;
+        callback.call(null,BadgerCache.cached_contacts);
+      });
+    }
+  }
+  
+};
